7e6391a1654cb1d65277d5a4b8f798406c16a3a5
sdhackney <sdhackney@protonmail.com>
2024-01-27 12:46:29 -0800

    added histogram, scatter, and checkbox

--- lot.py
+++ lot.py
@@ -5,5 +5,14 @@ import plotly.express as px
 df = pd.read_csv('vehicles_us.csv')
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
-st.header('Josh is GAY')
+st.header('Vehicle Data Analysis')
 st.dataframe(df)
+
+if st.checkbox('Show Price Distribution Histogram'):
+    
+    fig_hist = px.histogram(df, x='price', title='Price Distribution')
+    st.plotly_chart(fig_hist)
+
+st.subheader("Price vs Odometer Reading")
+fig_scatter = px.scatter(df, x='odometer', y='price', title='Price vs Odometer Reading')
+st.plotly_chart(fig_scatter)
