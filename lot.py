4a8b1491842b5ef7c91399fb2f6998f99dcb34b7
sdhackney <sdhackney@protonmail.com>
2024-01-27 10:14:20 -0800

    update .py

--- lot.py
+++ lot.py
@@ -5,5 +5,5 @@ import plotly.express as px
 df = pd.read_csv('vehicles_us.csv')
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
-st.header('Data viewer')
+st.header('Josh is GAY')
 st.dataframe(df)
